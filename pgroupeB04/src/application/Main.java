b8253611dcbf09b6cdcd5c04b9189ff3f93dd331
Vincent <Vincent@192.168.1.28>
2019-03-09 14:51:14 +0100

    Ajout musique

--- pgroupeB04/src/application/Main.java
+++ pgroupeB04/src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.nio.file.Paths;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
 import view.AddQuestion;
 import view.WelcomeMenu;
@@ -19,6 +23,12 @@ public class Main extends Application {
 			WelcomeMenu welM = new WelcomeMenu();
             AddQuestion addQ = new AddQuestion();
             welM.setId("welcome");
+            
+            //musique du jeu
+            AudioClip audioClip = new AudioClip(Paths.get("QVGDM.wav").toUri().toString());
+	        int volume = 2;
+	        Platform.runLater(() -> audioClip.play(volume));
+	        Thread.sleep(10);
             
             Scene scene = new Scene(welM, 1000, 600);primaryStage.setFullScreen(true);
             primaryStage.setFullScreenExitHint("Press Esc to exit full screen");
