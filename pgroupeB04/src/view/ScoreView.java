b4c1ca220f42e85ec0e45c4f7fd260a2d6fc4321
Vincent <Vincent@Vincent>
2019-03-14 12:06:43 +0100

    Add CSS ScoreView

--- pgroupeB04/src/view/ScoreView.java
+++ pgroupeB04/src/view/ScoreView.java
@@ -7,26 +7,54 @@ import javafx.scene.layout.AnchorPane;
 public class ScoreView extends AnchorPane{
 	
 	private Label lblWon;
+	private Label moneyWon;
+	private Label lblCongratulation;
+
 
 	public ScoreView() {
 		
 		this.setPadding(new Insets(10));
 
-		this.getChildren().addAll(getLblWon());
+		this.getChildren().addAll(getLblWon(),getMoneyWon(),getLblCongratulation());
 		
 		AnchorPane.setTopAnchor(getLblWon(), 200.);
-		AnchorPane.setRightAnchor(getLblWon(), 700.);
-		AnchorPane.setLeftAnchor(getLblWon(), 700.);
+		AnchorPane.setRightAnchor(getLblWon(), 610.);
+		
+		
+		AnchorPane.setBottomAnchor(getMoneyWon(), 450.);
+		AnchorPane.setRightAnchor(getMoneyWon(), 660.);
+		
+		AnchorPane.setBottomAnchor(getLblCongratulation(), 100.);
+		AnchorPane.setRightAnchor(getLblCongratulation(), 350.);
+		
 	}
 
 	public Label getLblWon() {
 		if(lblWon==null) {
-			lblWon=new Label(" You won !");
+			lblWon=new Label(" You won : ");
 			lblWon.setId("lblWon");
+			lblWon.setMinWidth(700);
+
 		}
 		return lblWon;
 	}
-	
-	
+	public Label getMoneyWon() {
+		if(moneyWon==null) {
+			moneyWon=new Label(" moneyWon ");
+			moneyWon.setId("moneyWon");
+			moneyWon.setMinWidth(500);
+
+		}
+		return moneyWon;
+	}
+	public Label getLblCongratulation() {
+		if(lblCongratulation==null) {
+			lblCongratulation=new Label(" CONGRATULATIONS ! ");
+			lblCongratulation.setId("lblCongratulation");
+			lblCongratulation.setMinWidth(1200);
+			
+		}
+		return lblCongratulation;
+	}
 
 }
