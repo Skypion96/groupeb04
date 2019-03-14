d0bb599ce2843ebb0de1b117088877d33c07aaaa
Vincent <Vincent@Vincent>
2019-03-14 10:26:33 +0100

    Add ScoreView

--- pgroupeB04/src/view/ScoreView.java
+++ pgroupeB04/src/view/ScoreView.java
@@ -4,25 +4,29 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
-public class ScoreView extends AnchorPane {
-
+public class ScoreView extends AnchorPane{
+	
 	private Label lblWon;
 
 	public ScoreView() {
-
+		
 		this.setPadding(new Insets(10));
 
 		this.getChildren().addAll(getLblWon());
-
-		AnchorPane.setTopAnchor(lblWon, 200.);
-		AnchorPane.setRightAnchor(getLblWon(), 200.);
+		
+		AnchorPane.setTopAnchor(getLblWon(), 200.);
+		AnchorPane.setRightAnchor(getLblWon(), 700.);
+		AnchorPane.setLeftAnchor(getLblWon(), 700.);
 	}
 
 	public Label getLblWon() {
-		if (lblWon == null) {
-			lblWon = new Label(" You won !");
+		if(lblWon==null) {
+			lblWon=new Label(" You won !");
+			lblWon.setId("lblWon");
 		}
 		return lblWon;
 	}
+	
+	
 
 }
