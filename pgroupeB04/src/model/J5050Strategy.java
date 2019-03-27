e9e2a1cff7734b4c09e1e09e025194d528325084
Vincent <Vincent@Vincent>
2019-03-27 15:22:01 +0100

    Add JokerStrategy

---
+++ pgroupeB04/src/model/J5050Strategy.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.List;
+import java.util.Map;
+
+import view.GameView;
+
+public class J5050Strategy implements IJokerStrategy{
+	
+	public void useJoker() {
+		
+		Map<String, Boolean> choices;
+		List<Question> questionAll;
+		String question;
+		
+		//question = GameView.getGame().getCurrentQuestion().toString();
+		
+		//questionAll.addAll(get)
+		
+		
+	}
+
+}
